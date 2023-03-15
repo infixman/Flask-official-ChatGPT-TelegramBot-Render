f884cecabd9716783406e3657871c9ee83ef9bac
Eason <eason@seasalt.ai>
2023-03-15 16:30:16 +0800

    feat: test print log

--- app.py
+++ app.py
@@ -5,20 +5,21 @@ import telegram
 from flask import Flask, request
 from telegram.ext import Dispatcher, MessageHandler, Filters
 
-app = Flask(__name__)
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
 CHAT_LANGUAGE = os.getenv("INIT_LANGUAGE", default = "zh")
 TELEGRAM_BOT_TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
 TG_BOT = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 DISPATCHER = Dispatcher(TG_BOT, None)
 CONVERSATIONS = {}
+LOG_LEVEL = os.getenv("LOG_LEVEL", default = "DEBUG")
+app = Flask(__name__)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=LOG_LEVEL)
+logger = logging.getLogger(__name__)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class ChatGPT:
-    def __init__(self, tg_user_id: int):
+    def __init__(self, tg_user_id: str):
         self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
         self.tg_user_id = tg_user_id
 
@@ -38,10 +39,12 @@ class ChatGPT:
         user_messages.append({"role": "assistant", "content": chatGPT_anserwer})
         CONVERSATIONS[self.tg_user_id] = user_messages
         
-        logger.info(f"""tg user: {self.tg_user_id})
+        log_msg = f"""tg user: {self.tg_user_id})
 👨‍💼:{user_input}
-🤖:{chatGPT_anserwer}""")
-        
+🤖:{chatGPT_anserwer}"""
+        print(log_msg)
+        logger.info(log_msg)
+
         return chatGPT_anserwer
 
 
@@ -58,9 +61,12 @@ def webhook_handler():
 
 def reply_handler(bot, update):
     """Reply message."""
-    chatgpt = ChatGPT(update.message.from_user.id)
-    ai_reply_response = chatgpt.get_response(update.message.text)
-    update.message.reply_text(ai_reply_response)
+    user_id = str(update.message.from_user.id)
+    print(f"user_id: {user_id}, message: {update.message.text}")
+    chatgpt = ChatGPT(user_id)
+    chatGPT_anserwer = chatgpt.get_response(update.message.text)
+    print(f"user_id: {user_id}, message: {update.message.text}, chatGPT: {chatGPT_anserwer}")
+    update.message.reply_text(chatGPT_anserwer)
 
 if __name__ == "__main__":
     DISPATCHER.add_handler(MessageHandler(Filters.text, reply_handler))
