c79e97734086bea0b925b3adac16b5aadbbb4257
Eason <eason@seasalt.ai>
2023-03-15 16:09:03 +0800

    fix: add `user` wher create ChatCompletion

--- app.py
+++ app.py
@@ -1,107 +1,64 @@
 import logging
-
-import telegram, os
+import openai
+import os
+import telegram
 from flask import Flask, request
 from telegram.ext import Dispatcher, MessageHandler, Filters
 
-
-
-#################
-import openai
-	
-openai.api_key = os.getenv("OPENAI_API_KEY") 
-
-
-chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
-	
-	
-conversation = []
-
-class ChatGPT:  
-    
-
-    def __init__(self):
-        
-        self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
-
-    def get_response(self, user_input):
-        conversation.append({"role": "user", "content": user_input})
-        
-
-        response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
-        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-        
-        print("AI回答內容：")        
-        print(response['choices'][0]['message']['content'].strip())
-
-
-        
-        return response['choices'][0]['message']['content'].strip()
-	
-
-
-
-
-
-#####################
-
-telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
-
-
-
-# Load data from config.ini file
-#config = configparser.ConfigParser()
-#config.read('config.ini')
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Initial Flask app
 app = Flask(__name__)
 
-# Initial bot by Telegram access token
-bot = telegram.Bot(token=telegram_bot_token)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+CHAT_LANGUAGE = os.getenv("INIT_LANGUAGE", default = "zh")
+TELEGRAM_BOT_TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+TG_BOT = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+DISPATCHER = Dispatcher(TG_BOT, None)
+CONVERSATIONS = {}
+
+class ChatGPT:
+    def __init__(self, tg_user_id: int):
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+        self.tg_user_id = tg_user_id
+
+    def get_response(self, user_input):
+        user_messages = []
+        if self.tg_user_id in CONVERSATIONS:
+            user_messages = CONVERSATIONS.get(self.tg_user_id)
+
+        user_messages.append({"role": "user", "content": user_input})
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages = user_messages,
+            user=str(self.tg_user_id),
+        )
+
+        chatgpt_anserwer = response['choices'][0]['message']['content'].strip()
+        user_messages.append({"role": "assistant", "content": chatgpt_anserwer})
+        CONVERSATIONS[self.tg_user_id] = user_messages
+        logger.info(f"{chatgpt_anserwer}")
+        return chatgpt_anserwer
 
 
 @app.route('/callback', methods=['POST'])
 def webhook_handler():
     """Set route /hook with POST method will trigger this method."""
     if request.method == "POST":
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        update = telegram.Update.de_json(request.get_json(force=True), TG_BOT)
 
         # Update dispatcher process that handler to process this message
-        dispatcher.process_update(update)
+        DISPATCHER.process_update(update)
     return 'ok'
 
 
 def reply_handler(bot, update):
     """Reply message."""
-
-    chatgpt = ChatGPT()        
-    
-                                             #人類的問題
-    ai_reply_response = chatgpt.get_response(update.message.text) #ChatGPT產生的回答
-    
-    update.message.reply_text(ai_reply_response) #用AI的文字回傳
-
-# New a dispatcher for bot
-dispatcher = Dispatcher(bot, None)
-
-# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# message.
-dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+    chatgpt = ChatGPT(update.message.from_user.id)
+    ai_reply_response = chatgpt.get_response(update.message.text)
+    update.message.reply_text(ai_reply_response)
 
 if __name__ == "__main__":
-    # Running server
+    DISPATCHER.add_handler(MessageHandler(Filters.text, reply_handler))
     app.run(debug=True)
+
